9c0108ee11a316f733a5dd5a77fb18e5774c925f
GDev96 <frattini.gaia@libero.it>
2024-02-23 18:12:43 +0100

    Update
    Add js value skills

---
+++ script.js
@@ -0,0 +1,15 @@
+//Percentuali skill
+document.addEventListener('DOMContentLoaded', function() {
+    const nameskills = document.querySelectorAll('.nameskill');
+    nameskills.forEach(function(nameskill) {
+        const originalText = nameskill.innerText; // Salva il testo originale
+        const newText = nameskill.dataset.newText; // Ottieni il nuovo testo dall'attributo data
+
+        nameskill.addEventListener('mouseover', function() {
+            this.textContent = newText;
+        });
+        nameskill.addEventListener('mouseout', function() {
+            this.textContent = originalText;
+        });
+    });
+});
